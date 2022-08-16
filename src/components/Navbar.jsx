10ba73df1651c93b08fe795ec6b91b9f44bb5303
priscilamattos <priiica12@gmail.com>
2022-08-16 16:36:21 -0400

    Offers page.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
-import { useNavigate, useLocation } from "react-router-dom";
-import { ReactComponent as OfferIcon } from "../assets/svg/localOfferIcon.svg";
+import { useLocation, useNavigate } from "react-router-dom";
+
 import { ReactComponent as ExploreIcon } from "../assets/svg/exploreIcon.svg";
+import { ReactComponent as OfferIcon } from "../assets/svg/localOfferIcon.svg";
 import { ReactComponent as PersonOutlineIcon } from "../assets/svg/personOutlineIcon.svg";
 
 const Navbar = () => {
@@ -46,7 +47,7 @@ const Navbar = () => {
                   : "navbarListName"
               }
             >
-              Offer
+              Offers
             </p>
           </li>
           <li className="navbarListItem" onClick={() => navigate("/profile")}>
